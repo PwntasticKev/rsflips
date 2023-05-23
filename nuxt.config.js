6854912b73b2f61a1432df263dd557750d4fb753
PwntasticKev <pwntastickevin@gmail.com>
2023-05-23 09:55:33 -0600

    filters

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,6 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  target: 'static',
-  generate: {
-    dir: 'dist'
-  },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
