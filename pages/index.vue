a54cfaacecccfd510929bb7f713f50881676b82e
PwntasticKev <pwntastickevin@gmail.com>
2023-06-01 10:43:02 -0600

    will be fixing this issue later

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <v-data-table
+      v-if="loading"
+      :headers="headers"
+      :items="loading ? skeletonData : pricingData"
+      :loading="loading"
+    >
+      <template v-slot:items>
+        <tr>
+          <td colspan="3">
+            <v-skeleton-loader :rows="5" type="table"></v-skeleton-loader>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
     <price-table
       :key="key"
       @refetch-data="
@@ -22,6 +36,14 @@ export default {
   },
   data() {
     return {
+      loading: true,
+      pricingData: [], // Actual pricing data from API
+      skeletonData: Array(5).fill({}), // Skeleton data for loading state
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'Price', value: 'price' },
+        { text: 'Quantity', value: 'quantity' }
+      ],
       key: 0
     };
   },
@@ -30,9 +52,10 @@ export default {
   },
   async asyncData({ store }) {
     await store.dispatch('pricingData/getMappingData');
-    await store.dispatch('pricingData/getPricingData');
   },
   async created() {
+    await this.getPricingData();
+    this.loading = false;
     this.interval = setInterval(() => {
       this.getPricingData();
     }, 180000);
